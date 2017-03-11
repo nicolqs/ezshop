eb840bce0e9d3e27c2eae36ab20969dbab8cefd0
dakaugu <dwightaugustin@Dwights-MacBook-Pro.local>
2017-03-11 15:19:38 -0800

    app styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../ez_logo.png';
 import ItemList from './ItemList';
 import CategoryList from './CategoryList';
 import './App.css';
@@ -9,11 +9,9 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to eZ Shop</h2>
+          <img src={logo}  alt="logo" height="50" />
         </div>
         <p className="App-intro">
-          Hello World
         </p>
         <ItemList />
         <CategoryList />
